7672a55605cfc6bc53ba1df885326ad41aea3730
Maxime MÉDAL <medal.max@gmail.com>
2024-02-07 15:28:58 +0100

    modfi de l'env

--- MysteryTest_Front/src/environments/environment.development.ts
+++ MysteryTest_Front/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7075/api' // TODO URL l'API
+    // apiUrl: 'https://localhost:7075/api' // TODO URL l'API
+    apiUrl: 'http://localhost:5189/api'
 };
--- MysteryTest_Front/src/environments/environment.ts
+++ MysteryTest_Front/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://localhost:7075/api'
+    // apiUrl: 'https://localhost:7075/api'
+    apiUrl: 'http://localhost:5189/api'
 };
